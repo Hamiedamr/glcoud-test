04310cc941603a2ac94ca50a20036be82343f70d
Abdelhamied Amr <abdelhamied.amr@outlook.com>
2022-12-09 02:39:39 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Hello gCloud")
+from google.cloud import vision
+from google.cloud.vision import types
+
+client = vision.ImageAnnotatorClient()
+
+
+respone=client.label_detection(image="__path__")
+labels=response.label_annotations
+print(label)
